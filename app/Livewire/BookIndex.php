b01e2f2d0c66c8ff1aa43d648308762ed8d44abe
Hasan <alltimehasan2@gmail.com>
2023-12-08 00:38:38 +0600

    Diving Starting Into Form

--- app/Livewire/BookIndex.php
+++ app/Livewire/BookIndex.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class BookIndex extends Component
 {
+    public string $title = '';
+
+    public function createBook()
+    {
+        dd($this->title);
+    }
+
     public function render()
     {
         return view('livewire.book-index', [
--- resources/views/livewire/book-index.blade.php
+++ resources/views/livewire/book-index.blade.php
@@ -1,5 +1,16 @@
-<div class="space-y-2">
-    @foreach ($books as $book)
-        <livewire:book-item :book="$book" wire:key="$book->id" />
-    @endforeach
+<div>
+    <div class="bg-slate-100 my-4 p-3 rounded-sm shadow-md">
+        <div class="p-6 text-gray-900">
+            <form wire:submit.prevent="createBook">
+                <input type="text" id="title" wire:model="title">
+                <button type="submit">Create Book</button>
+            </form>
+        </div>
+    </div>
+
+    <div class="space-y-2">
+        @foreach ($books as $book)
+            <livewire:book-item :book="$book" wire:key="$book->id" />
+        @endforeach
+    </div>    
 </div>
